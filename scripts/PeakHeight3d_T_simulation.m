6cfe5c1e6f785e610d0eed4ac3d006e52a4e162b
Fabian Telschow <fabian.telschow@posteo.de>
2018-11-06 22:54:57 +0100

    some changes in the plot

--- scripts/PeakHeight3d_T_simulation.m
+++ scripts/PeakHeight3d_T_simulation.m
@@ -41,20 +41,22 @@ if strcmp(machine, 'server')
     path      = '/home/ftelschow/PeakDetection/tmp/';
     % path where the simulations get saved
     path_sim  = '/home/ftelschow/PeakDetection/simulations/';
+    path_pics = '/home/ftelschow/PeakDetection/PeakDetection/pics/';
     % path to the mask of the simulated fields (will be generated later)
     path_mask = '/home/ftelschow/PeakDetection/mask.nii';
     % workspace
     cd /home/ftelschow/PeakDetection/tmp/;
 
 else
-    data_name = 'isoL505030nsim1n5000_gauss_stddev5';
-    %data_name = 'isoL505030nsim12000n1_gauss_stddev7.mat';
-    path_data = strcat('C:\Users\ftelschow\Documents\Linux\Research\MatlabCode\PeakDetection\data\',data_name);
-    path      = 'C:\Users\ftelschow\Documents\Linux\Research\MatlabCode\PeakDetection\tmp\';
-    path_sim  = 'C:\Users\ftelschow\Documents\Linux\Research\MatlabCode\PeakDetection\simulations\';
-    path_mask = 'C:\Users\ftelschow\Documents\Linux\Research\MatlabCode\PeakDetection\mask.nii';
+    %data_name = 'isoL505030nsim1n5000_gauss_stddev5';
+    data_name = 'isoL505030nsim12000n1_gauss_stddev7.mat';
+    path_data = strcat('data/',data_name);
+    path      = 'tmp/';
+    path_sim  = 'simulations/';
+    path_mask = '';
+    path_pics = 'pics/';
     % workspace
-    cd C:\Users\ftelschow\Documents\Linux\Research\MatlabCode\PeakDetection\tmp\;
+    cd /home/drtea/Research/MatlabPackages/STEM;
 end
 
 % Decide whether data needs to be pre-computed or is already saved on the 
@@ -82,23 +84,25 @@ else
     kernel =  'gauss'; % 'quartic'; %
 
     % sigmas for smoothing kernel
-    stddev = [5 5 5];
+    stddev = [5 5 5]; % [7 7 7];
 end
 % get dimension of the field
 D         = length(dim);
 
 % General Parameter for simulation
 Nvec      = [ 50 100 200 ]; %  [ 20 ];%      % sample size used to estimate LKCs, kappas etc
-Uvec      = 2.5; %[2.0 2.5 3.0]; %
-Msim      = 1e5;       % number of simulations
+Uvec      = [2.5 3.0]; %[2.0 2.5 3.0]; %
+Msim      = 1e4;       % number of simulations
 kappa     = 1;         % divide lower bound of integral by std in CS method (Y/N)
 FWHM      = stddev(1)*2*sqrt(2*log(2));
 dim1      = dim-1;
 R         = [1 sum(dim1)/FWHM ...
             (dim1(1)*dim1(2)+dim1(1)*dim1(3)+dim1(2)*dim1(3))/FWHM^2 ...
-            prod(dim1)/FWHM^3]; % True resels using the formula in K.J. Worsley et al. / NeuroImage 23 (2004) S189–S195
-fieldTYPE = 'Z';       % 'T'; %
-STAT      = 'Z';       % Type of statistic maxima are evaluated on (CS only supports 'Z')
+            prod(dim1)/FWHM^3]; % True resels using the formula in K.J. Worsley et al. / NeuroImage 23 (2004) S189ï¿½S195
+fieldTYPE = 'T'; %'Z';       %
+STAT      = 'T'; % 'Z';       % Type of statistic maxima are evaluated on (CS only supports 'Z')
+transformT2Z = 0;      % Option to transform the T field to a Z field before applying peak detection
+
 
 % % Construct a file for the mask needed in SPM code
 % nii_img = make_nii(ones(dim));
@@ -110,34 +114,34 @@ STAT      = 'Z';       % Type of statistic maxima are evaluated on (CS only supp
 % end
 
 %% Simulate p-values of local maxima higher than ui
-%sf = size(f);
+sf = size(f);
 %s = zeros( sf(1:D) );
 
 % Estimate the resels from a large number of fields
-%if ~strcmp( fieldTYPE, 'T')
-%     Mtest = 3000;
-%     Z = f( :, :, :, 1:Mtest );
-%     % transform data into a NIFTI file in order to process it in SPM
-%     for j = 1:Mtest
-%         nii_img = make_nii( Z(:, :, :, j) );
-%         save_nii(nii_img, strcat(num2str(j),'im.nii' ));
-%     end
-% 
-%     dinfo = dir(path);
-%     names = {dinfo.name};
-%     names = names(3:end);
-% 
-%     % Estimate resels using SPM
-%     [~,~,R] = spm_est_smoothness( char(names), path_mask, [500 500]);
-%     
-%     %%%
-%     % Estimate LKC using hermite estimator
-%     % Thresholds
-%     du = 0.05;
-%     u = -6:du:6;
-%     EC = EulerChar(Z(:,:,:,1:1000), u, 3);
-%     hEEC = HermiteEEC(EC, u, 3);
-%end
+if ~strcmp( fieldTYPE, 'T')
+    Mtest = 3000;
+    Z = f( :, :, :, 1:Mtest );
+    % transform data into a NIFTI file in order to process it in SPM
+    for j = 1:Mtest
+        nii_img = make_nii( Z(:, :, :, j) );
+        save_nii(nii_img, strcat(num2str(j),'im.nii' ));
+    end
+
+    dinfo = dir(path);
+    names = {dinfo.name};
+    names = names(3:end);
+
+    % Estimate resels using SPM
+    [~,~,Rest] = spm_est_smoothness( char(names), path_mask, [3000 3000]);
+    
+    %%%
+    % Estimate LKC using hermite estimator
+    % Thresholds
+    du = 0.05;
+    u = -6:du:6;
+    EC = EulerChar(Z(:,:,:,1:1000), u, 3);
+    hEEC = HermiteEEC(EC, u, 3);
+end
 for uu = 1:length(Uvec)
     ui = Uvec(uu);
     % Precompute the p-value Table for exact Gaussian overshoot distribution
@@ -148,6 +152,8 @@ for uu = 1:length(Uvec)
         % Initialize known variables
         lpval = 1;           % will become vector saving length of pvals
         N = Nvec(kk);
+        % get cummulative distribution functions for quantile transform
+        
         tic
         for NN = 1:Msim
             % Generate field and get degrees of freedom
@@ -157,6 +163,7 @@ for uu = 1:length(Uvec)
                      % Compute the T statistic
                      s  = sqrt( sum(Z(:, :, :, 1:N ).^2, D+1) / N );
                      T  = Z(:, :, :, N+1) ./ s;
+                     
     %                  mZ = mean( Z(:, :, :, 1:N ), D+1);
     %                  for i = 1:N
     %                      Z(:,:,:,i) = (Z(:,:,:,i) - mZ) ./ s ;
@@ -205,6 +212,17 @@ for uu = 1:length(Uvec)
                 end
             end
             [T, Loc ]= find_locMax( T, ui );
+            % transformation to Gaussianize the T-field
+            if transformT2Z
+                 sT   = size(T);
+                 Tvec = reshape( T, [1 prod(sT)] );
+                 for l =1:length(Tvec)
+                     Tvec(l) = -norminv(tcdf(-Tvec(l), N));
+                 end
+                 T = reshape(Tvec, sT);
+            end
+            clear l
+            
             % Compute the p-values with different methods
             if NN==1
                Ps       = SPM_peakFDR( 0.05, [N N], STAT, R, 1, T, ui, Loc );
@@ -229,7 +247,7 @@ for uu = 1:length(Uvec)
         end
         toc
 
-        output_name = strcat('FieldTYPE_',fieldTYPE,'_N',int2str(N),'Msim',int2str(Msim),'_isotropicGauss', num2str(stddev(1)), num2str(stddev(2)), num2str(stddev(3)),'kappa',num2str(kappa),'_prethresh',num2str(ui));
+        output_name = strcat('FieldTYPE_',fieldTYPE,'_N',int2str(N),'Msim',int2str(Msim),'_isotropicGauss', num2str(stddev(1)), num2str(stddev(2)), num2str(stddev(3)),'kappa',num2str(kappa),'_prethresh',num2str(ui),'_transform', num2str(transformT2Z));
         save( strcat(path_sim,output_name,'.mat'), 'Ps_spm', 'Ps_CS', 'Ps_CH', 'Ps_A', 'kernel', 'stddev','kappa','noise','Msim', 'Nvec')
     end
 end
@@ -240,14 +258,16 @@ end
 %% Generate plots of the results
 if ~strcmp(machine,'server')
     %%
+   %%
     set(groot, 'defaultAxesTickLabelInterpreter','latex'); set(groot, 'defaultLegendInterpreter','latex');
-     file ='FieldTYPE_T_N50Msim10000_isotropicGauss777kappa1_prethresh2.5';% output_name;
-    %file ='FieldTYPE_Z_N20Msim10000_isotropicGauss555kappa1_prethresh2';% output_name;
-    load(strcat('C:\Users\ftelschow\Documents\Linux\Research\MatlabCode\PeakDetection\simulations\',file,'.mat'))
-    path_pics = 'C:\Users\ftelschow\Documents\Linux\Research\MatlabCode\PeakDetection\pics\';
-
-    FieldTYPE = 'T';
-
+    % file ='FieldTYPE_Z_N50Msim10000_isotropicGauss555kappa1_prethresh2.5';
+    % file ='FieldTYPE_T_N50Msim10000_isotropicGauss777kappa1_prethresh2.5';% output_name;
+    % file ='FieldTYPE_T_N200Msim10000_isotropicGauss777kappa1_prethresh2.5_transform0';% output_name;
+    file ='FieldTYPE_Z_N20Msim10000_isotropicGauss777kappa1_prethresh3_transform0';% output_name;
+    load(strcat(path_sim,file,'.mat'))
+    FieldTYPE = 'Z';%  'T'; %'T2Z'; % 'T2Z'; % choose 'T2Z' if transform1==1
+    logPlot   = 0; % change both axis to log10 scale
+    
     figure(1), clf, set(gcf, 'Position', [ 300 300 600 600])
     subplot(2,2,1)
     histogram(Ps_CS, 'Normalization', 'pdf'); xlim([0 max([1 max(Ps_CS)])]); title('ChengExact')
@@ -269,45 +289,280 @@ if ~strcmp(machine,'server')
     xlabel('p-value'); set(gca, 'fontsize', 20);
     ylabel('probability')
 
-    saveas( gcf, strcat(path_pics,file,'_histogram.png') )
+  %  saveas( gcf, strcat(path_pics,file,'_histogram.png') )
 
-    figure(2), clf; hold on;
-if strcmp(FieldTYPE, 'Z')
+    figure(2), clf; hold on; set(gcf, 'Position', [ 300 300 800 600]);
+if strcmp(FieldTYPE, 'Z') || strcmp(FieldTYPE, 'T2Z')
     [F, x ] = ecdf(Ps_spm);
+    if logPlot
+        F = log10(F);
+        x = log10(x);
+        % remove first entries to make nice plots
+        F = F(2:end);
+        x = x(2:end);
+    end
     plot(x,F, 'color', 'red', 'linewidth', 1.5);
 
     [F, x ] = ecdf(Ps_CS);
+    if logPlot
+        F = log10(F);
+        x = log10(x);
+        % remove first entries to make nice plots
+        F = F(2:end);
+        x = x(2:end);
+    end
     plot(x,F, 'color', 'blue', 'linewidth', 1.5);
 else
     [F, x ] = ecdf(Ps_CS);
+    if logPlot
+        F = log10(F);
+        x = log10(x);
+        % remove first entries to make nice plots
+        F = F(2:end);
+        x = x(2:end);
+    end
     plot(x,F, 'color', 'blue', 'linewidth', 1.5);
     
     [F, x ] = ecdf(Ps_spm);
+    if logPlot
+        F = log10(F);
+        x = log10(x);
+        % remove first entries to make nice plots
+        F = F(2:end);
+        x = x(2:end);
+    end
     plot(x,F, 'color', 'red', 'linewidth', 1.5);
+    
 end
     % [F, x ] = ecdf(Ps_CA);
     % plot(x,F, 'color', 'cyan');
     if strcmp(FieldTYPE, 'Z')
         [F, x ] = ecdf(Ps_A);
+        if logPlot
+            F = log10(F);
+            x = log10(x);
+            % remove first entries to make nice plots
+            F = F(2:end);
+            x = x(2:end);
+        end
         plot(x,F, 'color', 'cyan', 'linewidth', 1.5);
     end
     
     [F, x ] = ecdf(Ps_CH);
+    if logPlot
+        F = log10(F);
+        x = log10(x);
+        % remove first entries to make nice plots
+        F = F(2:end);
+        x = x(2:end);
+    end
     plot(x,F, 'color', 'green', 'linewidth', 1.5);
 
-    plot([0 1], [0 1], 'color', 'black', 'linewidth', 1.5);
-    xlim([0, 0.05]), ylim([0, 0.05])
-    if strcmp(FieldTYPE, 'Z')
-        legend('SPM12', 'Exact', 'Adler (1981)', 'Chumbley', 'Uniform', 'Location', 'southeast' );
+    plot([-10 10], [-10 10], 'color', 'black', 'linewidth', 1.5);
+    if logPlot
+        xlim([-3.5, log10(1) ]), ylim([-3.5, log10(1) ])
+        plot([log10(0.05) log10(0.05)], [-10 10], '--k', 'linewidth', 1.5);
     else
-        legend( 'Exact (Gaussian)', 'SPM12 (t-stat)','Chumbley (t-stat)','Uniform', 'Location', 'southeast' );
+        xlim([0, 0.05]), ylim([0, 0.05])
     end
-    xlabel('p-value'); set(gca, 'fontsize', 24);
-    ylabel('empirical cdf')
+    if strcmp(FieldTYPE, 'Z')
+        %legend('SPM12', 'Exact', 'Adler (1981)', 'Chumbley', 'Uniform', 'Location', 'southeast' );
+        legend('GKF Ratio', 'Exact', 'Adler (1981)', 'GKF-1 Ratio', 'Uniform', 'Location', 'southeast' );
+    elseif strcmp(FieldTYPE, 'T2Z')
+        %legend( 'SPM12 (Gaussian)', 'Exact (Gaussian)', 'Chumbley (Gaussian)','Uniform', 'Location', 'southeast' );
+        legend( 'GKF Ratio (Gaussian)', 'Exact (Gaussian)', 'GKF-1 Ratio (Gaussian)','Uniform', 'Location', 'southeast' );
+        set(legend, 'fontsize', 20);
+    else
+        %legend( 'Exact (Gaussian)', 'SPM12 (t-stat)','Chumbley (t-stat)','Uniform', 'Location', 'southeast' );
+        legend( 'Exact (Gaussian)', 'GKF Ratio (t-stat)','GKF-1 Ratio (t-stat)','Uniform', 'Location', 'southeast' );
+    end
+    if logPlot
+        xlabel('log10(p-value)'); set(gca, 'fontsize', 24);
+        ylabel('log10(empirical cdf)')
+    else
+        xlabel('p-value'); set(gca, 'fontsize', 24);
+        ylabel('empirical cdf')        
+    end
+
     hold off;
-    saveas( gcf, strcat(path_pics, file,'_ecdf.png') )
+    saveas( gcf, strcat(path_pics, file,'_log',num2str(logPlot),'_ecdf.png') )
     
+    %%
+    set(groot, 'defaultAxesTickLabelInterpreter','latex'); set(groot, 'defaultLegendInterpreter','latex');
+    % Log or usual plot
+    logPlot = 0;
+    
+    figure(1), clf, set(gcf, 'Position', [ 300 300 600 600])
+    subplot(1,3,1)
+    file ='FieldTYPE_T_N50Msim10000_isotropicGauss777kappa1_prethresh-20_transform1';
+    load(strcat(path_sim,file,'.mat'))
+    histogram(Ps_CS, 'Normalization', 'pdf'); xlim([0 max([1 max(Ps_CS)])]); title('N=50')
+    xlabel('p-value'); set(gca, 'fontsize', 20);
+    ylabel('probability')
+
+    subplot(1,3,2)
+    file ='FieldTYPE_T_N100Msim10000_isotropicGauss777kappa1_prethresh-20_transform1';
+    load(strcat(path_sim,file,'.mat'))
+    histogram(Ps_CS, 'Normalization', 'pdf'); xlim([0 max([1 max(Ps_CS)])]); title('N=100')
+    xlabel('p-value'); set(gca, 'fontsize', 20);
+    ylabel('probability');
+
+    subplot(1,3,3)
+    file ='FieldTYPE_T_N200Msim10000_isotropicGauss777kappa1_prethresh-20_transform1';
+    load(strcat(path_sim,file,'.mat'))
+    histogram(Ps_CS, 'Normalization', 'pdf'); xlim([0 max([1 max(Ps_CS)])]); title('N=200')
+    xlabel('p-value'); set(gca, 'fontsize', 20);
+    ylabel('probability')
+
+    saveas( gcf, strcat(path_pics, 'FieldTYPE_T_Msim10000_isotropicGauss777kappa1_prethresh-20_transform1_histogram.png' ))
+
+    figure(2), clf; hold on;
+    file ='FieldTYPE_T_N50Msim10000_isotropicGauss777kappa1_prethresh-20_transform1';
+    load(strcat(path_sim,file,'.mat'))  
+    [F, x ] = ecdf(Ps_CS);
+    if logPlot
+        F = log10(F);
+        x = log10(x);
+        % remove first entries to make nice plots
+        F = F(2:end);
+        x = x(2:end);
+    end
+    plot(x,F, 'color', 'red', 'linewidth', 1.5);
+    
+    file ='FieldTYPE_T_N100Msim10000_isotropicGauss777kappa1_prethresh-20_transform1';
+    load(strcat(path_sim,file,'.mat'))  
+    [F, x ] = ecdf(Ps_CS);
+    if logPlot
+        F = log10(F);
+        x = log10(x);
+        % remove first entries to make nice plots
+        F = F(2:end);
+        x = x(2:end);
+    end
+    plot(x,F, 'color', 'blue', 'linewidth', 1.5);
+    
+    file ='FieldTYPE_T_N200Msim10000_isotropicGauss777kappa1_prethresh-20_transform1';
+    load(strcat(path_sim,file,'.mat'))  
+    [F, x ] = ecdf(Ps_CS);
+     if logPlot
+        F = log10(F);
+        x = log10(x);
+        % remove first entries to make nice plots
+        F = F(2:end);
+        x = x(2:end);
+    end
+    plot(x,F, 'color', 'green', 'linewidth', 1.5);
+    
+    % Plot uniform
+    plot([-10 10], [-10 10], 'color', 'black', 'linewidth', 1.5);
+    if logPlot
+        xlim([-3.5, log10(1) ]), ylim([-3.5, log10(1) ])
+        plot([log10(0.05) log10(0.05)], [-10 10], '--k', 'linewidth', 1.5);
+    else
+        xlim([0, 0.05]), ylim([0, 0.05])
+    end
+    
+    legend( '$$N=50$$', '$$N=100$$', '$$N=200$$','Uniform', 'Location', 'southeast' );
+    set(legend, 'fontsize', 20);
+
+    if logPlot
+        xlabel('log10(p-value)'); set(gca, 'fontsize', 24);
+        ylabel('log10(empirical cdf)')
+    else
+        xlabel('p-value'); set(gca, 'fontsize', 24);
+        ylabel('empirical cdf')        
+    end
+    hold off;
+    saveas( gcf, strcat(path_pics, 'FieldTYPE_T_Msim10000_isotropicGauss777kappa1_prethresh-20_transform1_ecdf.png') )
+    %%
+    set(groot, 'defaultAxesTickLabelInterpreter','latex'); set(groot, 'defaultLegendInterpreter','latex');
+    path_pics = 'pics/';
+    
+    figure(1), clf, set(gcf, 'Position', [ 300 300 600 600])
+    subplot(1,3,1)
+    file ='FieldTYPE_T_N50Msim10000_isotropicGauss777kappa1_prethresh-20_transform0';
+    load(strcat(path_sim,file,'.mat'))
+    histogram(Ps_CS, 'Normalization', 'pdf'); xlim([0 max([1 max(Ps_CS)])]); title('N=50')
+    xlabel('p-value'); set(gca, 'fontsize', 20);
+    ylabel('probability')
+
+    subplot(1,3,2)
+    file ='FieldTYPE_T_N100Msim10000_isotropicGauss777kappa1_prethresh-20_transform0';
+    load(strcat(path_sim,file,'.mat'))
+    histogram(Ps_CS, 'Normalization', 'pdf'); xlim([0 max([1 max(Ps_CS)])]); title('N=100')
+    xlabel('p-value'); set(gca, 'fontsize', 20);
+    ylabel('probability');
+
+    subplot(1,3,3)
+    file ='FieldTYPE_T_N200Msim10000_isotropicGauss777kappa1_prethresh-20_transform0';
+    load(strcat(path_sim,file,'.mat'))
+    histogram(Ps_CS, 'Normalization', 'pdf'); xlim([0 max([1 max(Ps_CS)])]); title('N=200')
+    xlabel('p-value'); set(gca, 'fontsize', 20);
+    ylabel('probability')
+
+    saveas( gcf, strcat(path_pics, 'FieldTYPE_T_Msim10000_isotropicGauss777kappa1_prethresh-20_transform0_histogram.png' ))
+
+    figure(2), clf; hold on;
+    file ='FieldTYPE_T_N50Msim10000_isotropicGauss777kappa1_prethresh-20_transform0';
+    load(strcat(path_sim,file,'.mat'))  
+    [F, x ] = ecdf(Ps_CS);
+     if logPlot
+        F = log10(F);
+        x = log10(x);
+        % remove first entries to make nice plots
+        F = F(2:end);
+        x = x(2:end);
+    end
+    plot(x,F, 'color', 'red', 'linewidth', 1.5);
+    
+    file ='FieldTYPE_T_N100Msim10000_isotropicGauss777kappa1_prethresh-20_transform0';
+    load(strcat(path_sim,file,'.mat'))  
+    [F, x ] = ecdf(Ps_CS);
+    if logPlot
+        F = log10(F);
+        x = log10(x);
+        % remove first entries to make nice plots
+        F = F(2:end);
+        x = x(2:end);
+    end
+    plot(x,F, 'color', 'blue', 'linewidth', 1.5);
+    
+    file ='FieldTYPE_T_N200Msim10000_isotropicGauss777kappa1_prethresh-20_transform0';
+    load(strcat(path_sim,file,'.mat'))  
+    [F, x ] = ecdf(Ps_CS);
+    if logPlot
+        F = log10(F);
+        x = log10(x);
+        % remove first entries to make nice plots
+        F = F(2:end);
+        x = x(2:end);
+    end
+    plot(x,F, 'color', 'green', 'linewidth', 1.5);
+    
+    % Plot uniform
+    plot([-10 10], [-10 10], 'color', 'black', 'linewidth', 1.5);
+    if logPlot
+        xlim([-3.5, log10(1) ]), ylim([-3.5, log10(1) ])
+        plot([log10(0.05) log10(0.05)], [-10 10], '--k', 'linewidth', 1.5);
+    else
+        xlim([0, 0.05]), ylim([0, 0.05])
+    end
+    
+    legend( '$$N=50$$', '$$N=100$$', '$$N=200$$','Uniform', 'Location', 'southeast' );
+    set(legend, 'fontsize', 20);
+
+    if logPlot
+        xlabel('log10(p-value)'); set(gca, 'fontsize', 24);
+        ylabel('log10(empirical cdf)')
+    else
+        xlabel('p-value'); set(gca, 'fontsize', 24);
+        ylabel('empirical cdf')        
+    end
+    hold off;
+    saveas( gcf, strcat(path_pics, 'FieldTYPE_T_Msim10000_isotropicGauss777kappa1_prethresh-20_transform0_ecdf.png') )
+        
 end
+
 % subplot(1,2,2), hold on;
 % [f, x ] = ecdf(Ps_spm);
 % I = (x <= 0.3 );
