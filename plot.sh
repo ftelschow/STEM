1c4f1093b6bc0d76cf3d5da4cd0c80206adbf309
Fabian Telschow <fabian.telschow@posteo.de>
2018-12-17 23:03:27 +0100

    added missing figures

--- plot.sh
+++ plot.sh
@@ -5,9 +5,9 @@
 # create a file containing the simulations for the T plots
 ls simulations/*_T_*_prethresh[!-]* > Tsimulations.txt
 ls simulations/FieldTYPE_Z_Msim1000_* > Histsimulations.txt
-
+ls simulations/FieldTYPE_Z_N20Msim10000_* > Zsimulations.txt
 
 # plot the T-field simulations using matlab
 matlab -nodesktop -nosplash -r "run scripts/plotscript.m"
-rm Tsimulations.txt Histsimulations.txt
+rm Tsimulations.txt Histsimulations.txt Zsimulations.txt
 
